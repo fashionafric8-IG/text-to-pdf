9856ecee2dbf9e94406dd06f7c37acc75a5f25e9
Shahbaz Khan <shahbaz@gmail.com>
2026-05-01 09:27:13 +0500

    Initial production release of Text to PDF tool

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ from fpdf import FPDF
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "https://toolsflash.org",
+            "https://www.toolsflash.org"
+        ]
+    }
+})
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    return response
 
 @app.route('/')
 def index():
